b9e101c214299147978f930993baf78990368935
Sahil Maheshwari <sahilm1711@gmail.com>
2022-04-13 07:49:47 +0530

    Creating the template

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable({})
+
+export class AuthService{ 
+    login() {
+        return {msg:'I have logged in'};
+    }
+
+    signup() {
+        return {msg:'I have signed up'}
+    }
+}
